bf9ff864f9264f4a73f5934c08cf4db334584ebe
bru-daniella <bu.daniella@gmail.com>
2026-03-11 21:05:26 -0300

    aula3

---
+++ app/index.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={style.container}>
+      <Text>aii que delicia cara.</Text>
+    </View>
+  );
+}
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#021123"
+  }
+})
