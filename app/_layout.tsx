20134d9a90c67b332a123cfb2f99e474fb536add
bru-daniella <bu.daniella@gmail.com>
2026-03-25 21:24:07 -0300

    update theme

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import React from "react";
+import { MD3LightTheme, PaperProvider } from "react-native-paper";
+
+const tema = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: "#bb4e06",
+    secondary: "#021123",
+    background: "#bb069d",
+    surface: "#144480",
+    onPrimary: "#FFFFFF",
+  }
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <PaperProvider theme={tema}>
+      <Stack />
+    </PaperProvider>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -1,9 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from 'react';
+//import { router } from "expo-router";
+import { Image, StyleSheet, Text, View } from "react-native";
+import { Button, Snackbar, useTheme } from "react-native-paper";
 
+export default function Index() {
+  const theme = useTheme();
+  const [rodando, setRodando] = useState(false);
+  const [tempo, setTempo] = useState(1500);
+  const minutos = String(Math.floor(tempo / 60)).padStart(2, "0");
+  const segundos = String(tempo % 60).padStart(2, "0");
+  const [exibeMensagem, setExibeMensagem] = useState(false);
 
-function Cronometro(){
-  
   useEffect(() => {
     let intervalo;
 
@@ -13,33 +20,34 @@ function Cronometro(){
       }, 1000);
     }
 
-  return () => textoCronometro;
-  }, []);
-}
-
-export default function Index() {
-  const [rodando, setRodando] = useState(false);
-  
-  function alternar(){
-    setRodando(!rodando);
-  }
-
-  const [tempo, setTempo] = useState(1500);
-  const minutos = String(Math.floor(tempo / 60)).padStart(2, "0");
-  const segundos = String(tempo % 60).padStart(2, "0");
-
+  return () => clearInterval(intervalo);
+  }, [rodando, tempo]);
   
   return (
-    <View style={style.container}>
+    <View style={[style.container, {background: theme.colors.background}]}>
       <Image style={style.image} source={require('./relogio.png')} />
       <View style={style.actions}>
-      <Text style={style.timer}>
-        {minutos}:{segundos}
-      </Text>
-      <Pressable onPress={alternar}
-      style={rodando ? style.buttonStart : style.buttonStop}>
-        <Text style={style.textButton}>{rodando ? "Parar" : "Iniciar"}</Text>
-      </Pressable>
+        <Text style={style.timer}>
+          {minutos}:{segundos}
+        </Text>
+        <Button mode="contained" 
+            style={[rodando ? style.buttonStart : style.buttonStop,
+            {backgroundColor: rodando ? "#990000" : theme.colors.secondary}]}
+            lebelStyle={style.textButton}
+            onPress={() => {
+              setRodando(!rodando);
+              setExibeMensagem(true);
+            }}> 
+          {rodando ? "Pausar" : "Iniciar"} 
+        </Button>
+
+        <Snackbar
+          visible={exibeMensagem}
+          duration={1500}
+          onDismiss={() => setExibeMensagem(false)}
+        >
+          {rodando ? "Cronômetro Iniciado" : "Cronômetro pausado"}
+        </Snackbar>
     </View>
     <View style={style.footer}>
       <Text style={style.textfooter}>Curso de react</Text>
